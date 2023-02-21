48e296eb0e787452daaacd017bff09f6481ce263
Prakashkumarjena700 <prakashkumarjena367@gmail.com>
2023-02-21 11:48:26 +0530

    1st commit

---
+++ frontend/src/Router/AllRoutes.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Home from '../Components/Home/Home'
+
+
+
+export default function AllRoutes() {
+    return (
+        <Routes>
+            <Route path='/' element={<Home />} ></Route>
+        </Routes>
+    )
+}
