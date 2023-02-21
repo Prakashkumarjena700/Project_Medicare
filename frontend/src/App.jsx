f2d1c204cec262a36838e78aaef67b39b569d707
Malay-24 <malaybajpai24@gmailcom>
2023-02-21 22:33:06 +0530

    frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import AllRoutes from './Router/AllRoutes';
-
+// import Footer from "./Components/Home/Footer/Footer"
 function App() {
   return (
     <div className="App">
       <AllRoutes />
+     
+      {/* <Footer/> */}
     </div>
   );
 }
